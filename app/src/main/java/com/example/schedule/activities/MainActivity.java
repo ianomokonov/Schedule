fd62b97e3169bd656c9524e02589fe21010ed40f
Иван Номоконов <nomokonov.vana@gmail.com>
2020-03-16 23:34:57 +0300

    fix

--- app/src/main/java/com/example/schedule/activities/MainActivity.java
+++ app/src/main/java/com/example/schedule/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.schedule.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.schedule.R;
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Расписание ФУ");
 //        onClick(null);
     }
 
