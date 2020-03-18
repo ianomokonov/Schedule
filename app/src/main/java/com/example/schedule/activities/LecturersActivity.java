13e0a4ee3a6ca49b07aa4b734adb34caf2af5dd8
NOLedCorp <noledcorp@gmail.com>
2020-03-18 13:08:06 +0300

    Вывод расписания преподавателя

--- app/src/main/java/com/example/schedule/activities/LecturersActivity.java
+++ app/src/main/java/com/example/schedule/activities/LecturersActivity.java
@@ -1,13 +1,16 @@
 package com.example.schedule.activities;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.schedule.BasePage;
 import com.example.schedule.R;
-import com.example.schedule.SchedulerPage;
 import com.example.schedule.models.Datable;
+import com.example.schedule.models.SearchListItem;
+import com.example.schedule.models.SearchType;
+import com.example.schedule.requests.GetLecturerClassesRequest;
 
 public class LecturersActivity extends BasePage implements Datable {
 
@@ -35,5 +38,29 @@ public class LecturersActivity extends BasePage implements Datable {
         lectures.setImageResource(R.drawable.user_graduate_solid);
         rooms.setImageResource(R.drawable.door_open_solid_gray);
         roomsСapacity.setImageResource(R.drawable.tachometer_alt_solid_gray);
+    }
+
+    @Override
+    public void onSearchPanelClick(View v){
+        Intent intent = new Intent(this, SearchActivity.class);
+        intent.putExtra("type", SearchType.LECTURER);
+        intent.putExtra("saveData", super.currentDate);
+        intent.putExtra("returnClass", LecturersActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected SearchListItem setDefaultSearchItem(){
+        return null;
+    }
+
+    @Override
+    public void refreshList(){
+        if(searchListItem != null && currentDate != null){
+            GetLecturerClassesRequest request = new GetLecturerClassesRequest(subjectAdapter, this, subjects);
+            super.handleExpanded(request);
+            return;
+        }
+        super.setNoDataView(0);
     }
 }
